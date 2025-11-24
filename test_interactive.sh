026b635ea8184561fc7310fde9237ad594a9b919
Dingqing Yang <dingqingy@nvidia.com>
2025-11-24 11:54:17 -0800

    to test latest rc cont

--- test_interactive.sh
+++ test_interactive.sh
@@ -1,9 +1,15 @@
 MBRIDGE=/lustre/fsw/coreai_dlalgo_llm/dingqingy/MOE/Megatron-Bridge
 MEGATRONPATH=/lustre/fsw/coreai_dlalgo_llm/dingqingy/MOE/megatron-lm
 
-CONT=gitlab-master.nvidia.com:5005/dl/joc/nemo-ci/aot_build_muon_new/train:pipe.38348673-x86
+SRC_CONT=/lustre/fsw/coreai_dlalgo_llm/gdeng/cont_save/nemo2511rc7.sqsh
+DEST_CONT=/lustre/fsw/coreai_dlalgo_llm/dingqingy/MOE/cont_save/nemo2511rc7.sqsh
+# CONT=gitlab-master.nvidia.com:5005/dl/joc/nemo-ci/aot_build_muon_new/train:pipe.38348673-x86
 # CONT=/lustre/fsw/coreai_dlalgo_llm/dingqingy/MOE/Megatron-Bridge/containers/nemo25.09-update.sqsh
 ACCOUNT=coreai_dlalgo_llm
 PARTITION=batch
 
-srun -A $ACCOUNT -N1 -p $PARTITION --container-image=$CONT -J $ACCOUNT-test. --container-mounts="$MBRIDGE:/opt/Megatron-Bridge,$MEGATRONPATH:/opt/megatron-lm" --pty bash
+# read-only
+# srun -A $ACCOUNT -N1 -p $PARTITION --container-image=$CONT -J $ACCOUNT-test. --container-mounts="$MBRIDGE:/opt/Megatron-Bridge,$MEGATRONPATH:/opt/megatron-lm" --pty bash
+
+# persistent
+srun -A $ACCOUNT -N1 -p $PARTITION --container-image=$SRC_CONT --container-save=$DEST_CONT -J $ACCOUNT-test. --container-mounts="$MBRIDGE:/opt/Megatron-Bridge,$MEGATRONPATH:/opt/megatron-lm" --pty bash
--- test_run.sh
+++ test_run.sh
@@ -3,8 +3,9 @@ MBRIDGE=/lustre/fsw/coreai_dlalgo_llm/dingqingy/MOE/Megatron-Bridge
 MEGATRONPATH=/lustre/fsw/coreai_dlalgo_llm/dingqingy/MOE/megatron-lm
 # CONT=nvcr.io/nvidian/nemo:25.11.rc5
 # CONT=nvcr.io/nvidia/nemo:25.09
-CONT=gitlab-master.nvidia.com:5005/dl/joc/nemo-ci/aot_build_muon_new/train:pipe.38348673-x86
+# CONT=gitlab-master.nvidia.com:5005/dl/joc/nemo-ci/aot_build_muon_new/train:pipe.38348673-x86
 # CONT=/lustre/fsw/coreai_dlalgo_llm/dingqingy/MOE/Megatron-Bridge/containers/nemo25.09-update.sqsh
+CONT=/lustre/fsw/coreai_dlalgo_llm/dingqingy/MOE/cont_save/nemo2511rc7.sqsh
 ACCOUNT=coreai_dlalgo_llm
 PARTITION=batch
 
@@ -12,7 +13,7 @@ PARTITION=batch
 # PYTHONPATH=$MBRIDGE/src:$MBRIDGE/scripts/performance:$PYTHONPATH  \
 PYTHONPATH=$MEGATRONPATH:$MBRIDGE/src:$MBRIDGE/scripts/performance:$PYTHONPATH  \
 python -m scripts.performance.setup_experiment -m deepseek -s v3 --task pretrain --num_gpus 256 \
--a $ACCOUNT -p $PARTITION -l $NEMORUN_HOME -i $CONT -hf $HF_TOKEN -t "00:40:00" \
+-a $ACCOUNT -p $PARTITION -l $NEMORUN_HOME -i $CONT -hf $HF_TOKEN -t "00:30:00" \
 -cm "$MBRIDGE:/opt/Megatron-Bridge,$MEGATRONPATH:/opt/megatron-lm" \
 --gpu b200 \
 -gn 8 -en \
