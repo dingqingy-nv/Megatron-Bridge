86cf6f57e139247a84b511a0b3f6b77fd117a1cc
Dingqing Yang <dingqingy@nvidia.com>
2025-11-27 21:27:34 -0800

    B200 256 GPU qk_clip+muon+deepep 496 TFLOPS

--- scripts/performance/configs/kimi/kimi_llm_pretrain.py
+++ scripts/performance/configs/kimi/kimi_llm_pretrain.py
@@ -41,7 +41,7 @@ def set_kimi_k2_common_configs(cfg: ConfigContainer) -> None:
     cfg.ddp.grad_reduce_in_fp32 = False
 
     cfg.model.moe_router_force_load_balancing = True
-    cfg.model.qk_clip = False 
+    cfg.model.qk_clip = True 
 
 
 def kimi_k2_gb300_config(precision: str = "bf16") -> ConfigContainer:
@@ -135,8 +135,8 @@ def kimi_k2_b200_config(precision: str = "bf16") -> ConfigContainer:
         pipeline_parallelism=base_cfg.pipeline_model_parallel_size,
         virtual_pipeline_parallelism=base_cfg.virtual_pipeline_model_parallel_size,
         # moe_flex_dispatcher_backend=base_cfg.moe_flex_dispatcher_backend,
-        enable_deepep=False,
-        optimizer_type="adam",
+        enable_deepep=True,
+        optimizer_type="muon",
         # layout="Et|(tt|)*30mL",
     )
     set_kimi_k2_common_configs(cfg)
